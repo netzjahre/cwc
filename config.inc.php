ec8ae19deac81afe051455a4a69d5b5cfba05af0
JF <j.fercher@web.de>
2024-02-08 14:30:01 +0100

    Add files via upload
    your data

---
+++ config.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+//Append a copy of this section for each site
+
+//Site ID: choose unique consecutive ID starting from 1 (eg. 1, 2, 3, etc) 
+$id=1;
+//The site label
+$sitename[$id]="your.site";
+//MySQL database host
+$dbhost[$id]="your-server";
+//Database name
+$dbname[$id]="your-db";
+//Database user name
+$dbuser[$id]="your-username";
+//Database password
+$dbpass[$id]="your-password";
+//Table name
+$tablename[$id]="contatore";#example
+
+?>
+
